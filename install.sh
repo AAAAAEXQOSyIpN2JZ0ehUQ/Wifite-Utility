c5527116d9cf41afe76246c58e29658fefd58e42
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2024-09-08 16:42:53 -0300

    install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /opt
+sudo rm -rf Wifite-Utility
+sudo git clone https://github.com/AAAAAEXQOSyIpN2JZ0ehUQ/Wifite-Utility.git
+sudo chmod +x Wifite-Utility/*
+cd Wifite-Utility
+ls -lthas
+cd
+echo "cd /opt/Wifite-Utility && sudo ./Wifite-Utility.sh" > wifiteutility
+sudo chmod +x wifiteutility
+sudo mv wifiteutility /usr/local/bin/
+cd
