65c0642de9b28dc5c6a563be70ffbec9641a5f0d
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2024-10-19 06:15:47 -0300

    crear_wifite_desktop.sh

--- Install/crear_wifite_desktop.sh
+++ Install/crear_wifite_desktop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Nombre de la aplicación
-APP_NAME="GUI wifite "
+APP_NAME="GUI Wifite"
 
 # Ruta del ejecutable
 EXEC_PATH="/usr/share/kali-menu/exec-in-shell \"wifiteutility\""
@@ -9,13 +9,12 @@ EXEC_PATH="/usr/share/kali-menu/exec-in-shell \"wifiteutility\""
 # Ruta del ícono
 ICON_PATH="kali-wifite"
 
-# Archivo .desktop que vamos a crear
-# DESKTOP_FILE="~/.local/share/applications/guiwifite.desktop"
-DESKTOP_FILE="/usr/share/applications/guiwifite.desktop"
+# Rutas de los archivos .desktop
+APPLICATIONS_DESKTOP_FILE="/usr/share/applications/kali-guiwifite.desktop"
+KALI_MENU_DESKTOP_FILE="/usr/share/kali-menu/applications/kali-guiwifite.desktop"
 
-# Crear el archivo .desktop con el contenido necesario
-sudo bash -c "cat > $DESKTOP_FILE << EOL
-[Desktop Entry]
+# Contenido del archivo .desktop
+DESKTOP_ENTRY="[Desktop Entry]
 Name=$APP_NAME
 Comment=Python script to automate wireless auditing using aircrack-ng tools
 Encoding=UTF-8
@@ -25,14 +24,23 @@ StartupNotify=false
 Terminal=true
 Type=Application
 Categories=06-wireless-attacks;
-X-Kali-Package=wifite
-EOL"
+X-Kali-Package=wifite"
 
-# Asegurar los permisos adecuados para que el archivo sea accesible
-sudo chmod 644 $DESKTOP_FILE
+# Función para crear el archivo .desktop
+create_desktop_file() {
+    local file_path="$1"
+    echo "$DESKTOP_ENTRY" | sudo tee "$file_path" > /dev/null
+    sudo chmod 644 "$file_path"
+    echo "¡El archivo .desktop en $file_path se ha creado exitosamente!"
+}
 
-# Actualizar la base de datos del menú para que se reflejen los cambios
+# Crear el archivo .desktop en /usr/share/applications
+create_desktop_file "$APPLICATIONS_DESKTOP_FILE"
+
+# Crear el archivo .desktop en /usr/share/kali-menu/applications
+create_desktop_file "$KALI_MENU_DESKTOP_FILE"
+
+# Actualizar la base de datos del menú para reflejar los cambios
 sudo update-desktop-database
 
-echo "¡El archivo .desktop para $APP_NAME se ha creado exitosamente!"
-
+echo "Todos los archivos .desktop han sido creados y la base de datos del menú ha sido actualizada."
