a450f6df094729085cb7a831213da5603ca14c2e
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2024-11-29 04:49:58 -0300

    install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cd /opt
 sudo rm -rf Wifite-Utility
 sudo git clone https://github.com/AAAAAEXQOSyIpN2JZ0ehUQ/Wifite-Utility.git
 sudo chmod +x Wifite-Utility/*
-sudo chmod +x Wifite-Utility/Install/*
+sudo chmod +x Wifite-Utility/Install/* 
 cd Wifite-Utility
 ls -ltha
 cd
@@ -15,7 +15,7 @@ cd
 sudo apt-get -y update
 sudo apt-get install -y wifite
 sudo apt install -y hcxdumptool hcxtools
-sudo wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/Wifite-Utility/refs/heads/main/Install/crear_guiwifite_desktop.sh -O - | sudo bash
+sudo wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/Wifite-Utility/refs/heads/main/Install/crear_guiwifite_desktop.sh -O - | sudo bash && sudo rm -rf wget-log*
 
 # Fin del script
 
