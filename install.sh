5f6633bbc47a9fab6658b67cdab8db5a75d18b34
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2025-02-16 22:01:23 -0300

    install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-# Instalación
+## :computer: Instalación
 cd /opt
+sudo cp -r Wifite-Utility/hs /opt/hs_backup
+sudo cp Wifite-Utility/cracked.json /opt/cracked.json_backup
 sudo rm -rf Wifite-Utility
 sudo git clone https://github.com/AAAAAEXQOSyIpN2JZ0ehUQ/Wifite-Utility.git
+sudo cp -r /opt/hs_backup /opt/Wifite-Utility/hs
+sudo cp /opt/cracked.json_backup /opt/Wifite-Utility/cracked.json
 sudo chmod +x Wifite-Utility/*
 sudo chmod +x Wifite-Utility/Install/* 
 cd Wifite-Utility
 ls -ltha
 
-# Acceso Directo
+## :key: Acceso Directo
 cd
 echo "cd /opt/Wifite-Utility && sudo ./Wifite-Utility.sh" > gwifite
 echo "cd /opt/Wifite-Utility && sudo ./Wifite-Utility.sh" > xwifite
@@ -17,12 +21,13 @@ sudo chmod +x gwifite xwifite
 sudo mv gwifite xwifite /usr/local/bin/
 cd
 
-# Paquete
+## :package: Paquete
 sudo apt-get -y update
 sudo apt-get install -y wifite
 sudo apt install -y hcxdumptool hcxtools
 
-# Crear Desktop
+## :computer: Crear Desktop
+```bash
 sudo wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/Wifite-Utility/refs/heads/main/Install/crear_guiwifite_desktop.sh -O - | sudo bash && sudo rm -rf wget-log*
-
+```
 # Fin del script
